70df30bcef0617343087628033117cb9b50d5057
Kirill Iizumskii <mr.llirus@gmail.com>
2020-08-17 23:19:43 +0300

    Function

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+class Contact:
+
+    def __init__(self, name, surname, phone):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+
+        self.additional_information = {}
+
+
+    def __str__(self):
+
+        
+    # def add_information(self, email, add_num, social_network, *args, **kwargs):
+    #     self.email = email
+    #     self.add_num = add_num
+    #     self.social_network = social_network
+
+    favor = False
+
+
