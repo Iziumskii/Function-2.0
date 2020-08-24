04293a7b033f054f22225684caeffc4543bbadf2
Kirill Iizumskii <mr.llirus@gmail.com>
2020-08-24 21:25:34 +0300

    func fix

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+class PhoneBook:
+
+    def __init__(self, name):
+        self.name = name
+
+        self.contacts = []
+
+    # def show_contacts(self, contact):
+    #     self.contacts.values(contact)
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+        #
+        # def del_contact(self, contact):
+        #     self.contacts.popitem(contact)
+        #
+        # def find_favor_contacts(self, contact):
+        #     self.contacts.items(contact)
+        #
+        # def find_contact(self):
+        # pass
 
 
 class Contact:
@@ -9,15 +30,20 @@ class Contact:
 
         self.additional_information = {}
 
-
     def __str__(self):
+        return str(self.__dict__)
 
-        
-    # def add_information(self, email, add_num, social_network, *args, **kwargs):
-    #     self.email = email
-    #     self.add_num = add_num
-    #     self.social_network = social_network
+    def add_information(self, email, add_num, social_network, *args, **kwargs):
+        self.additional_information.update(email)
+        self.additional_information.update(add_num)
+        self.additional_information.update(social_network)
 
     favor = False
 
 
+book = PhoneBook('test')
+c1 = Contact('jora', 'sidorov', '+7918')
+
+book.add_contact(c1)
+print(book.contacts)
+print(c1)
