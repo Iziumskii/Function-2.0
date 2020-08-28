165b55b25a94dbeece075db4800e5a42dd08e451
Kirill Iizumskii <mr.llirus@gmail.com>
2020-08-28 23:43:19 +0300

    func fix func

--- main.py
+++ main.py
@@ -5,45 +5,55 @@ class PhoneBook:
 
         self.contacts = []
 
-    # def show_contacts(self, contact):
-    #     self.contacts.values(contact)
-
     def add_contact(self, contact):
         self.contacts.append(contact)
-        #
-        # def del_contact(self, contact):
-        #     self.contacts.popitem(contact)
-        #
-        # def find_favor_contacts(self, contact):
-        #     self.contacts.items(contact)
-        #
-        # def find_contact(self):
-        # pass
+
+    def show_contact(self, contact):
+        if contact in self.contacts:
+            return contact
+
+    def del_contact(self, contact):
+        if contact in self.contacts:
+            self.contacts.remove(contact)
+
+    def find_contact(self, name):
+        pass
+        # if name == Contact.__name__:
+        #     return Contact.__name__
+
+    def find_favor_contact(self, fav):
+        pass
 
 
 class Contact:
 
-    def __init__(self, name, surname, phone):
+    def __init__(self, name, surname, phone, *args, **kwargs):
         self.name = name
         self.surname = surname
         self.phone = phone
+        self.inf = args, kwargs
 
-        self.additional_information = {}
+    def favor(self):
+        favor = False
+        if not favor:
+            return 'нет'
+        else:
+            return 'да'
 
     def __str__(self):
-        return str(self.__dict__)
+        return str(f'Имя:{self.name} \nФамилия:{self.surname} \nТелефон:{self.phone} \nВ избранных:{self.favor()}'
+                   f' \nДополнительная информация:\n {self.inf}')
 
-    def add_information(self, email, add_num, social_network, *args, **kwargs):
-        self.additional_information.update(email)
-        self.additional_information.update(add_num)
-        self.additional_information.update(social_network)
-
-    favor = False
 
 
 book = PhoneBook('test')
-c1 = Contact('jora', 'sidorov', '+7918')
+jhon = Contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
+alex = Contact('Alex', 'Bolt', '+74683126987', telegram='@Allexx', email='alex@bolt.com')
 
-book.add_contact(c1)
+book.add_contact(jhon)
+book.add_contact(alex)
+
+#print(book.show_contact(alex))
+#book.del_contact(alex)
+
 print(book.contacts)
-print(c1)
